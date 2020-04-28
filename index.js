cd71a062fe38d3deb9b682111c788be750829ec0
MH <Mohammadhossein73@yahoo.com>
2020-04-29 01:20:59 +0430

    edit attribute

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // This is a sample file for the purpose of deming Git Attributes
-const API_KEY = "21hfzichuzisvbszi2";
+const API_KEY = "{SECURE_API_KEY}";
 const makeAPICall = function(key){
 	console.log("Making API Call ...")
 }
+makeAPICall(API_KEY);
